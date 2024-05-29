b3ac35ebfe7075b803c0f3ff2beeb06ba1c01303
klu_2200031618 <kk258786@gmail.com>
2024-05-29 20:37:26 +0530

    In place of infinite scroll, another styling is done, need to upload other projects

--- src/pages/main.js
+++ src/pages/main.js
@@ -13,15 +13,19 @@ export const Main=()=>{
                     favorite movies and TV shows.</p>
                     <a rel='noreferrer' href='github.com' target={'_blank'}>Github Link</a><br></br>
                     <div className='project-photo-background' id='project_photo_background'>
+                        <div className='blur-left-left'></div>
+                        <div className='blur-left'></div>
+                        <div className='project-photo-left-section'>Scroll Right ⇛</div>
                         <a rel='noreferrer' target={'_blank'} href={require('./images/project1/preview1-img1.png')}><img alt='preview-img1' className='project-photo' src={require('./images/project1/preview1-img1.png')} /></a>
                         <a rel='noreferrer' target={'_blank'} href={require('./images/project1/preview1-img2.png')}><img alt='preview-img1' className='project-photo' src={require('./images/project1/preview1-img2.png')} /></a>
                         <a rel='noreferrer' target={'_blank'} href={require('./images/project1/preview1-img3.png')}><img alt='preview-img1' className='project-photo' src={require('./images/project1/preview1-img3.png')} /></a>
                         <a rel='noreferrer' target={'_blank'} href={require('./images/project1/preview1-img4.png')}><img alt='preview-img1' className='project-photo' src={require('./images/project1/preview1-img4.png')} /></a>
                         <a rel='noreferrer' target={'_blank'} href={require('./images/project1/preview1-img5.png')}><img alt='preview-img1' className='project-photo' src={require('./images/project1/preview1-img5.png')} /></a>    
+                        <div className='project-photo-right-section'>⇚ Scroll Left</div>
+                        <div className='blur-right-right'></div>
+                        <div className='blur-right'></div>
                     </div>
                 </div>
-                <div className='blur-left-left'></div>
-                <div className='blur-left'></div>
             </div>
         </div>
     </div>
