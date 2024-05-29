e074a640bf076690428e8b574e074911f696efe4
klu_2200031618 <kk258786@gmail.com>
2024-05-29 16:38:58 +0530

    The theory entering part of a project is completed and need to complete the portion of photos and need to complete the logic of infinite horizontal scroll

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,7 +3,27 @@ import './allPages.css'
 export const Main=()=>{
     return(
     <div className='main-background'>
-        Hello from Main page
+        <div className='main-mini-background'>
+            <div className='project-main-container'>
+                <div className='each-project-container'>
+                    <h1>Movie TV Show Recommendation System</h1>
+                    <p>-Our Movie and TV Show Recommendation System empowers users to seamlessly 
+                    access information on specific movies, createpersonalized watchlists, and enjoy a smooth user experience.Offering a 
+                    curated selection of content, our system enhancesentertainment discovery, making it easy for users to find their 
+                    favorite movies and TV shows.</p>
+                    <a rel='noreferrer' href='github.com' target={'_blank'}>Github Link</a><br></br>
+                    <div className='project-photo-background' id='project_photo_background'>
+                        <a rel='noreferrer' target={'_blank'} href={require('./images/project1/preview1-img1.png')}><img alt='preview-img1' className='project-photo' src={require('./images/project1/preview1-img1.png')} /></a>
+                        <a rel='noreferrer' target={'_blank'} href={require('./images/project1/preview1-img2.png')}><img alt='preview-img1' className='project-photo' src={require('./images/project1/preview1-img2.png')} /></a>
+                        <a rel='noreferrer' target={'_blank'} href={require('./images/project1/preview1-img3.png')}><img alt='preview-img1' className='project-photo' src={require('./images/project1/preview1-img3.png')} /></a>
+                        <a rel='noreferrer' target={'_blank'} href={require('./images/project1/preview1-img4.png')}><img alt='preview-img1' className='project-photo' src={require('./images/project1/preview1-img4.png')} /></a>
+                        <a rel='noreferrer' target={'_blank'} href={require('./images/project1/preview1-img5.png')}><img alt='preview-img1' className='project-photo' src={require('./images/project1/preview1-img5.png')} /></a>    
+                    </div>
+                </div>
+                <div className='blur-left-left'></div>
+                <div className='blur-left'></div>
+            </div>
+        </div>
     </div>
     )
 }
